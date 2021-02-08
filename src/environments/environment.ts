4b8c74e038076fc56ae3b0ef3e6b3c77c2b4b8de
ThabangManaka <thabangdansteyn@gmail.com>
2021-02-08 10:02:28 +0200

    "Added firebase config"

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false ,
+  firebaseConfig : {
+    apiKey: "AIzaSyB1OojENs4ocsVWhI-bM9aR909xTNRxU3c",
+    authDomain: "sellingapp-ea6b0.firebaseapp.com",
+    projectId: "sellingapp-ea6b0",
+    storageBucket: "sellingapp-ea6b0.appspot.com",
+    messagingSenderId: "616954994039",
+    appId: "1:616954994039:web:892b73b25b475138f91572",
+    measurementId: "G-QSHR54C9Y5"
+  }
 };
 
 /*
