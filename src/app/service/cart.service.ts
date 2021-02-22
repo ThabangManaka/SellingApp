22d8b730cde7f7a17579eeb564c1b55f681201cc
ThabangManaka <thabangdansteyn@gmail.com>
2021-02-22 14:57:56 +0200

    Added Capacitor

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable} from "rxjs";
+import { CartModel } from '../model/ICartModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+
+  private cartDataArray: CartModel = {
+    count: 0,
+    productData: []
+}
+
+  private cartData$ = new BehaviorSubject<CartModel>({count: 0, productData: []});
+  private totalAmount = 0;
+  private totalAmount$ = new BehaviorSubject<number>(0);
+
+
   constructor() { }
+
 
   addToCart(product) {
     console.log(product)
