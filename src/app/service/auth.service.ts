fee830a0c2d61d499f632c1cb47c001ed02037bc
ThabangManaka <thabangdansteyn@gmail.com>
2021-02-24 16:07:55 +0200

    Added localstorage

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,25 +3,35 @@ import { Register } from '../IRegister';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { SecureStorageService } from './secure-storage.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
   constructor(private afAuth: AngularFireAuth,
     private firestore: AngularFirestore,
-    private router : Router) {
+    private router : Router,
+    private secureStorageService :SecureStorageService) {
       this.afAuth.authState.subscribe(user => {
-        console.log(user.uid)
+        if (user) {
+          this.firestore.collection('users').doc(user.uid).valueChanges().subscribe(res=> {
+            this.secureStorageService.set('user',res);
+          })
+        //  localStorage.setItem('user',JSON.stringify(this.user));
+             }else {
+              this.secureStorageService.set('user',null);
+             }
+
       })
     }
 
 
     async login(login : Login) {
       var result = await this.afAuth.signInWithEmailAndPassword(login.email,login.password).then(res =>{
-        console.log(res)
-        this.router.navigateByUrl('/tabs/tab1');
+        console.log(res);
+        this.router.navigateByUrl('tabs/tab1');
 
       }).catch(res => {
         console.log(res);
@@ -31,7 +41,6 @@ export class AuthService {
     register(user :Register, password: string) {
 
       this.afAuth.createUserWithEmailAndPassword(user.email,password).then(res => {
-
 
         this.addUser(res.user.uid, user)
 
@@ -48,6 +57,9 @@ export class AuthService {
 
   onLogout() {
     this.afAuth.signOut();
+    //localStorage.removeItem('user');
+    this.secureStorageService.remove('user');
+    this.router.navigate(['login']);
   }
   forgotpassword(passwordResetEmail) {
     return this.afAuth.sendPasswordResetEmail(passwordResetEmail)
@@ -57,6 +69,15 @@ export class AuthService {
       }).catch((error) => {
         window.alert(error)
       });
+  }
+  get isLoggedIn(): boolean {
+  const user =  this.secureStorageService.get('user');
+    return  user  !==  null;
+  }
+
+
+   get currentUser(): any {
+    return (this.afAuth.authState !== null) ? this.afAuth.authState : null;
   }
 
 }
